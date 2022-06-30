af3cbf8bde110f45676e3c549a095174efaadc90
lizethalbarracin <anyisitapte13@gmail.com>
2022-06-30 10:05:15 -0500

    prueba final

--- routes/users.router.js
+++ routes/users.router.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const CategoriesService = require('./../services/categories.service');
+const validatorHandler = require('./../middlewares/validator.handler');
+const { createCategorytSchema, updateCategorySchema, getCategorySchema } = require('./../schemas/category.schema');
 
 const router = express.Router();
+const service = new CategoriesService();
 
 router.get('/', (req, res) => {
   const { limit, offset } = req.query;
@@ -15,4 +19,60 @@ router.get('/', (req, res) => {
 });
 
 
+
+router.get('/:categoryId/products/:productId', (req, res) => {
+  const { categoryId, productId } = req.params;
+  res.json({
+    categoryId,
+    productId,
+  });
+})
+
+router.get('/', async (req, res) => {
+  const categorys = await service.find();
+  res.json(categorys);
+});
+
+router.get('/:id',
+  validatorHandler(getCategorySchema, 'params'),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const Category = await service.findOne(id);
+      res.json(Category);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+router.post('/',
+  validatorHandler(createCategorytSchema, 'body'),
+  async (req, res) => {
+    const body = req.body;
+    const newCategory = await service.create(body);
+    res.status(201).json(newCategory);
+  }
+);
+
+router.patch('/:id',
+  validatorHandler(getCategorySchema, 'params'),
+  validatorHandler(updateCategorySchema, 'body'),
+  async (req, res, next) => {
+    try{
+      const { id } = req.params;
+      const body = req.body;
+      const category = await service.update(id, body);
+      res.json(category);
+    } catch (error) {
+      next(error);
+    }
+  })
+
+  router.delete('/:id', async (req, res) => {
+    const { id } = req.params;
+    const rta = await service.delete(id);
+    res.json(rta);
+  });
+  
 module.exports = router;
